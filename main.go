d9b087bddeaa6cf83f6512a746001cb81c043d89
Mark <mark@Hatlam-MBP.local>
2019-12-05 01:30:08 +0300

    Added balancing algorithm configuration: round-robin, weighted-round-robin, least-connections, weighted-least-connections

--- main.go
+++ main.go
@@ -2,29 +2,45 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"sort"
 	"strconv"
 	"sync"
 	"time"
+
+	"github.com/jmcvetta/randutil"
 )
 
 type Configuration struct {
-	ServerList []string `json:"server_list"`
-	Protocol   string   `json:"ecosystem_protocol"`
-	Port       int      `json:"load_balancer_port"`
-	Timeout    int      `json:"server_check_timeout"`
+	ServerList []*Endpoint `json:"server_list"`
+	Protocol   string      `json:"ecosystem_protocol"`
+	Port       int         `json:"load_balancer_port"`
+	Delay      int         `json:"server_check_delay"`
+	Timeout    int         `json:"server_check_timeout"`
+	ProxyMode  string      `json:"proxy_mode"`
+	Algorithm  string      `json:"balancing_algorithm"`
+}
+
+type Endpoint struct {
+	URL    string  `json:"endpoint"`
+	Weight float64 `json:"weight"`
 }
 
 type Server struct {
-	URL   *url.URL
-	Alive bool
-	mux   sync.RWMutex
-	Proxy *httputil.ReverseProxy
+	URL               *url.URL
+	Weight            float64
+	Index             int
+	ActiveConnections int
+	Alive             bool
+	mux               sync.RWMutex
+	Proxy             *httputil.ReverseProxy
 }
 
 func (server *Server) GetAlive() bool {
@@ -57,12 +73,47 @@ type ServerPool struct {
 	Current    int
 }
 
+func (pool *ServerPool) GetPoolChoice() []randutil.Choice {
+	choice := []randutil.Choice{}
+	for _, server := range pool.ServerList {
+		if server.GetAlive() {
+			weight := int(server.Weight * 100)
+			if (weight > 0) && (weight < 1) {
+				weight = 1
+			}
+			choice = append(choice, randutil.Choice{Weight: weight, Item: server.Index})
+		}
+	}
+	return choice
+}
+
+func (pool *ServerPool) GetWeightedLeastConnectedServer() *Server {
+	serverList := pool.ServerList
+	sort.Slice(serverList, func(i, j int) bool {
+		if (math.Max(float64(serverList[i].ActiveConnections), 1) / serverList[i].Weight) < (math.Max(float64(serverList[j].ActiveConnections), 1) / serverList[j].Weight) {
+			return true
+		}
+		return false
+	})
+
+	fmt.Println(serverList[0])
+	return serverList[0]
+}
+
+func (pool *ServerPool) GetLeastConnectedServer() *Server {
+	serverList := pool.ServerList
+	sort.Slice(serverList, func(i, j int) bool {
+		return serverList[i].ActiveConnections < serverList[j].ActiveConnections
+	})
+	return serverList[0]
+}
+
 func (pool *ServerPool) AddServer(server *Server) {
 	pool.ServerList = append(pool.ServerList, server)
 }
 
 func (pool *ServerPool) NextPool() int {
-	current := 0
+	var current int
 	if (pool.Current + 1) > (len(pool.ServerList) - 1) {
 		pool.Current = 0
 		current = pool.Current
@@ -77,12 +128,32 @@ func (pool *ServerPool) NextPool() int {
 }
 
 func loadBalance(w http.ResponseWriter, r *http.Request) {
-	index := pool.NextPool()
-	pool.ServerList[index].Proxy.ServeHTTP(w, r)
+	switch configuration.Algorithm {
+	case "round-robin":
+		index := pool.NextPool()
+		endpoint := pool.ServerList[index]
+		endpoint.Proxy.ServeHTTP(w, r)
+	case "weighted-round-robin":
+		poolChoice := pool.GetPoolChoice()
+		server, _ := randutil.WeightedChoice(poolChoice)
+		index := server.Item.(int)
+		endpoint := pool.ServerList[index]
+		endpoint.Proxy.ServeHTTP(w, r)
+	case "least-connections":
+		endpoint := pool.GetLeastConnectedServer()
+		endpoint.ActiveConnections = endpoint.ActiveConnections + 1
+		endpoint.Proxy.ServeHTTP(w, r)
+		endpoint.ActiveConnections = endpoint.ActiveConnections - 1
+	case "weighted-least-connections":
+		endpoint := pool.GetWeightedLeastConnectedServer()
+		endpoint.ActiveConnections = endpoint.ActiveConnections + 1
+		endpoint.Proxy.ServeHTTP(w, r)
+		endpoint.ActiveConnections = endpoint.ActiveConnections - 1
+	}
 }
 
 func serversCheck() {
-	timer := time.NewTicker(time.Second * 10)
+	timer := time.NewTicker(time.Duration(configuration.Delay) * time.Second)
 	for {
 		select {
 		case <-timer.C:
@@ -103,17 +174,20 @@ func main() {
 	}
 	json.Unmarshal(file, &configuration)
 
-	for _, serverURL := range configuration.ServerList {
-		serverURL, err := url.Parse(configuration.Protocol + "://" + serverURL)
+	for index, server := range configuration.ServerList {
+		serverURL, err := url.Parse(configuration.Protocol + "://" + server.URL)
 		if err != nil {
 			log.Fatal(err)
 		}
 		proxy := httputil.NewSingleHostReverseProxy(serverURL)
 
 		pool.AddServer(&Server{
-			URL:   serverURL,
-			Alive: true,
-			Proxy: proxy,
+			URL:               serverURL,
+			Weight:            server.Weight,
+			ActiveConnections: 0,
+			Index:             index,
+			Alive:             true,
+			Proxy:             proxy,
 		})
 	}
 
