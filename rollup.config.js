314a6decb41749d4b0494b0bbfc19009eea40b01
Mark <shalpack@gmail.com>
2020-12-14 10:46:14 +0300

    Include sourcemaps in dev mode; enable terser plugin in production mode

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,24 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
- 
+import { terser } from 'rollup-plugin-terser';
+
+const DEVELOPMENT = process.env.DEVELOPMENT
+
 const config = {
   input: 'content/assets/js/src/index.js',
   output: {
     dir: 'content/assets/js/dist/',
+    sourcemap: DEVELOPMENT,
     format: 'esm'
   },
   watch: {
     include: "content/assets/js/src/**"
   },
   plugins: [resolve(), commonjs()]
-};
- 
+}
+
+if (!DEVELOPMENT) {
+  config.plugins.push(terser())
+}
+
 export default config;
